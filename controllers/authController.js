0c33b5b0128de7647bb98acf210e1eb0277c877a
richCode <chineduogada@gmail.com>
2020-09-02 01:45:28 +0100

    feature(login): implement endpoint
    implement `login` endpoint in the backend
    
    [Finishes #2]

--- controllers/authController.js
+++ controllers/authController.js
@@ -57,3 +57,41 @@ exports.signup = catchAsync(async (req, res, next) => {
 	});
 });
 
+exports.login = catchAsync(async (req, res, next) => {
+	const schema = {
+		email: Joi.string().email().required(),
+		password: Joi.string().required(),
+	};
+
+	const { error } = Joi.validate(req.body, schema);
+	if (error) {
+		const err = new AppError(error.details[0].message, 400);
+		return next(err);
+	}
+
+	const user = await UserModel.findOne({ email: req.body.email }).select(
+		"+password"
+	);
+
+	if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
+		const err = new AppError("invalid `email` or `password`", 400);
+		return next(err);
+	}
+
+	const token = signToken({ id: user._id });
+
+	res.status(201).json({
+		status: "success",
+		token,
+		data: {
+			user: {
+				_id: user._id,
+				email: user.email,
+				first_name: user.first_name,
+				last_name: user.last_name,
+				phone_number: user.phone_number,
+			},
+		},
+	});
+});
+
